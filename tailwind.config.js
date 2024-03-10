20319c73d2062795b99220014a5190945c5e1e09
Matteo Gassend <mgassend@gmail.com>
2024-03-10 17:10:35 +0100

    add: tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: 'class',
+  content: ['resources/views/**/*.{edge|html|js|jsx|ts|tsx,vue}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
