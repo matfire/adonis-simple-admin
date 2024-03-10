e207b93cfa8281475a5a78dcd1c62a88a584869e
Matteo Gassend <mgassend@gmail.com>
2024-03-10 15:43:20 +0100

    fix: build stuff

--- src/controllers/admin_controller.ts
+++ src/controllers/admin_controller.ts
@@ -1,7 +1,21 @@
-import type { HttpContext } from '@adonisjs/core/http'
+import { HttpContext } from '@adonisjs/core/http'
+import app from '@adonisjs/core/services/app'
+import { Edge } from 'edge.js'
+import { SimpleAdminConfig } from '../types/main.js'
+
+declare module '@adonisjs/core/http' {
+  interface HttpContext {
+    /**
+     * Reference to the edge renderer to render templates
+     * during an HTTP request
+     */
+    view: ReturnType<Edge['createRenderer']>
+  }
+}
 
 export default class AdminController {
-  async index({ response }: HttpContext) {
-    return response.send('Hello, admin!')
+  async index({ view }: HttpContext) {
+    const config = await app.config.get<SimpleAdminConfig>('simple_admin')
+    return view.render(`${config.templateNamespace}::pages/index`)
   }
 }
