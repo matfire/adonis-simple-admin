891d051f985544a14d3049bbb5bc02ee30158de1
Matteo Gassend <mgassend@gmail.com>
2024-03-10 17:10:18 +0100

    add: helper for include paths

--- providers/simple_admin_provider.ts
+++ providers/simple_admin_provider.ts
@@ -1,16 +1,18 @@
 import { ApplicationService } from '@adonisjs/core/types'
 import edge from 'edge.js'
 import { SimpleAdminConfig } from '../src/types/main.js'
-const AdminController = () => import('../src/controllers/admin_controller.js')
+import AdminController from '../src/controllers/admin_controller.js'
 
 export default class SimpleAdminProvider {
   constructor(protected app: ApplicationService) {}
   async boot() {
     const BASE_URL = new URL('./', import.meta.url)
-    const adminConfig = (await this.app.config.get('simple_admin')) as SimpleAdminConfig
+    const adminConfig = await this.app.config.get<SimpleAdminConfig>('simple_admin')
     const router = await this.app.container.make('router')
-    console.log(adminConfig)
     edge.mount(adminConfig.templateNamespace, new URL('../resources/views', BASE_URL))
+    edge.global('simpleAdminInclude', (value: string) => {
+      return `${adminConfig.templateNamespace}::${value}`
+    })
     router.get(adminConfig.path, [AdminController, 'index'])
   }
 }
